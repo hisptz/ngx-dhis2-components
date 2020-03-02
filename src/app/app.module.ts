d33ea3693826c1ca5cdd7fb93106089a05cef769
Rajab Mkomwa <rajeyw@gmail.com>
2020-03-03 00:08:42 +0300

    feat: add files for http client library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app.routing.module';
 import { containers } from './containers';
+import { NgxDhis2HttpClientModule } from 'projects/ngx-dhis2-http-client/src/public_api';
 
 @NgModule({
   declarations: [AppComponent, ...containers],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgxDhis2HttpClientModule.forRoot({
+      namespace: 'hisptz',
+      version: 1,
+      models: {
+        users: 'id',
+        organisationUnitLevels: 'id',
+        organisationUnits: 'id,name,level',
+        organisationUnitGroups: 'id',
+        dataStore_scorecards: 'id'
+      }
+    }),
     AppRoutingModule,
     MaterialModule
   ],
