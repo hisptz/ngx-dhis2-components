3df150670e72ff74b73ff5e86ff509dac0561c2b
Rajab Mkomwa <rajeyw@gmail.com>
2020-04-01 11:04:50 +0300

    refactor: remove indirect import of files

--- projects/ngx-dhis2-dictionary/src/lib/ngx-dhis2-dictionary.module.ts
+++ projects/ngx-dhis2-dictionary/src/lib/ngx-dhis2-dictionary.module.ts
@@ -1,17 +1,36 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { NgxPaginationModule } from 'ngx-pagination';
 
-import { components } from './components';
-import { containers } from './containers';
-import { pipes } from './pipes';
+import { DataElementGroupComponent } from './components/data-element-group/data-element-group.component';
+import { DataElementComponent } from './components/data-element/data-element.component';
+import { DataSetComponent } from './components/data-set/data-set.component';
+import { DictionaryMenuComponent } from './components/dictionary-menu/dictionary-menu.component';
+import { DictionaryProgressComponent } from './components/dictionary-progress/dictionary-progress.component';
+import { FunctionsComponent } from './components/functions/functions.component';
+import { IndicatorPropertiesComponent } from './components/indicator-properties/indicator-properties.component';
+import { IndicatorComponent } from './components/indicator/indicator.component';
+import { IndicatorsListComponent } from './components/indicators-list/indicators-list.component';
+import { MetadataListComponent } from './components/metadata-list/metadata-list.component';
+import { ProgramIndicatorPropertiesComponent } from './components/program-indicator-properties/program-indicator-properties.component';
+import { ProgramIndicatorComponent } from './components/program-indicator/program-indicator.component';
+import { ProgramIndicatorsComponent } from './components/program-indicators/program-indicators.component';
+import { SwitchingBtnsComponent } from './components/switching-btns/switching-btns.component';
+import { DictionaryListComponent } from './containers/dictionary-list/dictionary-list.component';
+import { FilterBySearchInputPipe } from './pipes/filter-by-search-input.pipe';
+import { FilterIndicatorsByGroupIdPipe } from './pipes/filter-indicators-by-group-id.pipe';
+import { SearchIndicatorGroupPipe } from './pipes/search-indicator-group.pipe';
+import { ShortenNamePipe } from './pipes/shorten-name.pipe';
 import { ExportService } from './services/export.service';
 import { IndicatorsService } from './services/indicators.service';
 import { DictionaryEffects } from './store/effects/dictionary.effects';
@@ -24,9 +43,6 @@ import {
   programIndicatorGroupsReducer,
   programIndicatorsListReducer
 } from './store/reducers/indicators.reducers';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
@@ -56,8 +72,29 @@ import { MatListModule } from '@angular/material/list';
     EffectsModule.forFeature([IndicatorsEffects])
   ],
 
-  declarations: [...components, ...containers, ...pipes],
-  exports: [...containers],
+  declarations: [
+    DataElementComponent,
+    DataElementGroupComponent,
+    DataSetComponent,
+    DictionaryProgressComponent,
+    FunctionsComponent,
+    MetadataListComponent,
+    ProgramIndicatorComponent,
+    IndicatorsListComponent,
+    IndicatorPropertiesComponent,
+    ProgramIndicatorPropertiesComponent,
+    ProgramIndicatorComponent,
+    ProgramIndicatorsComponent,
+    SwitchingBtnsComponent,
+    DictionaryMenuComponent,
+    IndicatorComponent,
+    DictionaryListComponent,
+    SearchIndicatorGroupPipe,
+    FilterBySearchInputPipe,
+    FilterIndicatorsByGroupIdPipe,
+    ShortenNamePipe
+  ],
+  exports: [DictionaryListComponent],
   providers: [DatePipe, IndicatorsService, ExportService]
 })
 export class NgxDhis2DictionaryModule {}
